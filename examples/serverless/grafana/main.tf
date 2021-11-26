4e099701af4a19c3e67dc07cb36597ab4d042691
Anna Muscarella <amuscarella@google.com>
2021-11-26 07:57:01 +0000

    replaced manual mirroring of Grafana repo with mirrored gcr repo

--- examples/serverless/grafana/main.tf
+++ examples/serverless/grafana/main.tf
@@ -1,5 +1,5 @@
 locals {
-  apis = ["iam.googleapis.com", "compute.googleapis.com", "run.googleapis.com", "apigateway.googleapis.com", "servicemanagement.googleapis.com", "servicecontrol.googleapis.com", "compute.googleapis.com", "iap.googleapis.com", "sql-component.googleapis.com", "cloudapis.googleapis.com", "containerregistry.googleapis.com", "sqladmin.googleapis.com", "artifactregistry.googleapis.com"]
+  apis = ["iam.googleapis.com", "compute.googleapis.com", "run.googleapis.com", "apigateway.googleapis.com", "servicemanagement.googleapis.com", "servicecontrol.googleapis.com", "iap.googleapis.com", "sql-component.googleapis.com", "cloudapis.googleapis.com", "sqladmin.googleapis.com"]
 }
 
 data "google_project" "project" {
@@ -13,27 +13,6 @@ resource "google_project_service" "project" {
 
   //disable_dependent_services = true
   disable_on_destroy = false
-}
-
-resource "google_artifact_registry_repository" "grafana" {
-  provider = google-beta
-
-  location = var.region
-  project = var.project_id
-  repository_id = "grafana"
-  description = "Docker repository for Grafana"
-  format = "DOCKER"
-}
-
-resource "null_resource" docker_image {
-
-  provisioner "local-exec" {
-    command = <<EOT
-docker pull grafana/grafana:${var.grafana_version}
-docker tag grafana/grafana:${var.grafana_version} ${var.region}-docker.pkg.dev/${var.project_id}/grafana/grafana:${var.grafana_version}
-docker push ${var.region}-docker.pkg.dev/${var.project_id}/grafana/grafana:${var.grafana_version}
-EOT
-  }
 }
 
 resource "google_cloud_run_service" "default" {
@@ -50,7 +29,7 @@ resource "google_cloud_run_service" "default" {
   template {
     spec {
       containers {
-        image ="${var.region}-docker.pkg.dev/${var.project_id}/grafana/grafana:${var.grafana_version}"
+        image ="mirror.gcr.io/grafana/grafana:latest"
         ports {
           name = "http1"
           container_port = 8080
@@ -145,7 +124,6 @@ resource "google_cloud_run_service" "default" {
 
   depends_on = [
     google_project_service.project,
-    null_resource.docker_image,
     google_sql_database.database,
     google_sql_user.user
   ]
